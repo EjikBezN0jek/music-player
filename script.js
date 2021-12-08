5c3efcd8aebd01d3866d70ba94364b91ee58553f
TanyaTrue <tatianatrue04@gmail.com>
2021-12-09 00:20:17 +0300

    added duration and current time.

--- script.js
+++ script.js
@@ -8,6 +8,8 @@ const  progress = document.getElementById("progress");
 const  prevBtn = document.getElementById("prev");
 const  playBtn = document.getElementById("play");
 const  nextBtn = document.getElementById("next");
+let currentTime = document.getElementById("current-time");
+let fullTime = document.getElementById("full-time");
 
 const songs = [
   'LanaDelRey-ArtDeco',
@@ -21,9 +23,11 @@ const songs = [
 
 let songIndex = 0;
 
-function formatSongTitle(item) {
-  return item.replace(/([^-)])([A-Z(])/g, '$1 $2').split('-');
-}
+loadSong(songs[songIndex]);
+
+/*
+*  Functions
+*/
 
 function loadSong(song){
   let title = formatSongTitle(song);
@@ -33,11 +37,32 @@ function loadSong(song){
   titleName.innerText = name;
   audio.src = `music/${song}.mp3`;
   cover.src = `images/${song}.jpg`;
-  cover.onload = () => cover.style.display = 'block';
+  cover.onload = () => {
+    cover.style.display = 'block';
+    cover.classList.remove('cover-animation');
+    setTimeout(() => cover.classList.add('cover-animation'), 10)
+  };
   cover.onerror = () => cover.style.display = 'none';
 }
 
-loadSong(songs[songIndex]);
+function setDuration({target}) {
+  fullTime.innerText = formatDuration(target.duration);
+}
+
+function updateProgress(e) {
+  let {duration, currentTime: current} = e.target;
+  const progressPercent = (current / duration) *100;
+  progress.style.width = `${progressPercent}%`;
+  currentTime.innerText = formatDuration(current);
+}
+
+function setProgress(e) {
+  const width = this.clientWidth;
+  const clickX = e.offsetX;
+  const duration = audio.duration;
+
+  audio.currentTime = (clickX / width)* duration;
+}
 
 function playSong() {
   musicContainer.classList.add("play");
@@ -54,32 +79,6 @@ function pauseSong() {
 
   audio.pause();
 }
-
-function updateProgress(e) {
-  const {duration, currentTime} = e.target;
-  const progressPercent = (currentTime / duration) *100;
-  progress.style.width = `${progressPercent}%`;
-}
-
-function setProgress(e) {
-  const width = this.clientWidth;
-  const clickX = e.offsetX;
-  const duration = audio.duration;
-
-  audio.currentTime = (clickX / width)* duration;
-}
-
-
-
-playBtn.addEventListener('click', () =>{
-  const  isPlaying = musicContainer.classList.contains("play");
-
-  if(isPlaying){
-    pauseSong();
-  } else {
-    playSong();
-  }
-});
 
 function prevSong() {
   songIndex--;
@@ -103,9 +102,40 @@ function nextSong() {
   playSong();
 }
 
+/*
+*  Events
+*/
+
+playBtn.addEventListener('click', () =>{
+  const  isPlaying = musicContainer.classList.contains("play");
+
+  if(isPlaying){
+    pauseSong();
+  } else {
+    playSong();
+  }
+});
 prevBtn.addEventListener('click', prevSong);
 nextBtn.addEventListener('click', nextSong);
 
+audio.addEventListener('canplaythrough', setDuration);
 audio.addEventListener('timeupdate', updateProgress);
 audio.addEventListener('ended', nextSong);
+
 progressContainer.addEventListener('click', setProgress);
+
+/*
+*  Utils
+*/
+
+function addZero(num) {
+  return num < 10 ? `0${num}` : num;
+}
+
+function formatDuration(sec) {
+  return addZero((sec / 60).toFixed()) + ':' + addZero((sec % 60).toFixed())
+}
+
+function formatSongTitle(item) {
+  return item.replace(/([^-)])([A-Z(])/g, '$1 $2').split('-');
+}
